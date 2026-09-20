cee0975466788b51e1685c86fc6be29c73309862
SpenDM <spencer.d.morris@gmail.com>
2026-09-20 06:59:49 -0700

    swap results image

--- public/js/main.js
+++ public/js/main.js
@@ -11,7 +11,7 @@ import { reopenDate, rounds } from './rounds.js';
 
 const $ = (sel) => document.querySelector(sel);
 
-const BANNER = { predict: 'resources/images/atami_banzuke.png', results: 'resources/images/onosato_win.png' };
+const BANNER = { predict: 'resources/images/atami_banzuke.png', results: 'resources/images/onosato_win_2.png' };
 let results = null;   // the ResultsView of the round picked in the Past Banzuke box
 let view = null;
 let schedule = [];
